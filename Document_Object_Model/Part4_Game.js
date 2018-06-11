3f55791118798fa8a790bd5d07fc9f1e2e536322
JohnRattz <jrattz94@gmail.com>
2018-06-11 14:51:43 -0400

    Finished DOM section

--- Document_Object_Model/Part4_Game.js
+++ Document_Object_Model/Part4_Game.js
@@ -1,35 +1,26 @@
-// Restart Game Button
-var restart = document.querySelector('#b');
-
-// Grab all the squares
-var squares = document.querySelectorAll("td");
-
-
-// Clear Squares Function
-function clearBoard() {
-  for (var i = 0; i < squares.length; i++) {
-      squares[i].textContent = '';
-  }
-
-}
-restart.addEventListener('click',clearBoard)
-
-
-
-
-// Create a function that will check the square marker
+// Method called when cell is clicked.
 function changeMarker(){
-    if(this.textContent === ''){
-      this.textContent = 'X';
-      // console.log(marker)
-    }else if (this.textContent === 'X') {
-      this.textContent = 'O';
-    }else {
-      this.textContent = '';
-    }
-};
-
-// Use a for loop to add Event listeners to all the squares
-for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', changeMarker);
+	if(this.textContent==''){
+		this.textContent = 'X';
+	} else if (this.textContent=='X'){
+		this.textContent = 'O';
+	} else {
+		this.textContent = '';
+	}
 }
+
+// Set click listeners on table cells.
+var squares = document.querySelectorAll('td');
+for (square of squares){
+	square.addEventListener('click', changeMarker);
+}
+
+// Metho called when restart button is clicked.
+function restart(){
+	for (square of squares){
+		square.textContent = '';
+	}
+}
+// Set click listener on restart button.
+var restart_btn = document.querySelector('#b');
+restart_btn.addEventListener('click', restart);
