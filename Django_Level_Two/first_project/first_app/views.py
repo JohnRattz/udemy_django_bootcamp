d26f6730f3eb4afa3d8c14bf92a5c5009fe788ab
JohnRattz <jrattz94@gmail.com>
2018-06-18 14:59:40 -0400

    Finished Django Level Two MTV Paradigm

--- Django_Level_Two/first_project/first_app/views.py
+++ Django_Level_Two/first_project/first_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_content': "Hello I'm from first_app!"}
-    return render(request, 'first_app/index.html', my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    # my_dict = {'insert_content': "Hello I'm from first_app!"}
+    return render(request, 'first_app/index.html', date_dict)
