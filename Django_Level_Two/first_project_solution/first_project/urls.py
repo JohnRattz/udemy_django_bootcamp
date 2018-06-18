8ee1488bae15407d5a3b7ac97b4ce1bcd7804668
JohnRattz <jrattz94@gmail.com>
2018-06-18 11:32:33 -0400

    Finished Django Level Two Introduction

--- Django_Level_Two/first_project_solution/first_project/urls.py
+++ Django_Level_Two/first_project_solution/first_project/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from first_app import views as first_app_views
+from django.conf.urls import include
+from first_app import views
 
 urlpatterns = [
-    url(r'^$', first_app_views.index, name='index'),
+    url(r'^$',views.index,name='index'),
+    url(r'^first_app/',include('first_app.urls')),
     url(r'^admin/', admin.site.urls),
-    # Examining first_app.urls shows that `/` and `/first_app` are the same.
-    url(r'first_app/', include('first_app.urls'))
 ]
